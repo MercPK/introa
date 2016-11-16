983fc6dbbde89593e4324249cca7ee3029479f1f
MercPK <coelhinhoninja@hotmail.com>
2016-11-16 11:12:45 -0500

    Update myjs.js

--- Example/myjs.js
+++ Example/myjs.js
@@ -1,7 +1,8 @@
 var imageArray = [
-	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/485976_491677310883441_2109765528_n.jpg?oh=2a1d379eee96e27335c5332235af60d6&oe=58D3940F",
-	"",
-	""
+	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/14956490_595140504026732_9047451617144161170_n.jpg?oh=ccb7a0ebab5d8827b8542f0f17504565&oe=588B5139",
+	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/12294885_803055099803225_1753496077626980062_n.jpg?oh=a39a8d7b82de4a30e1d273ddaaf55fff&oe=58CF2061",
+	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-0/p206x206/12733987_1967840493441025_4172856520855866935_n.jpg?oh=4b148401b48b56a75939377671776edf&oe=58C869B0","https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/485976_491677310883441_2109765528_n.jpg?oh=2a1d379eee96e27335c5332235af60d6&oe=58D3940F"
+];
 ];
 var currentIndex = 0;
 
