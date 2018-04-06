557fda1928911ec793d56c7104500d4abc9c89a5
MercPK <coelhinhoninja@hotmail.com>
2018-04-06 10:43:10 -0400

    Update myjs.js

--- website/myjs.js
+++ website/myjs.js
@@ -2,7 +2,6 @@ var imageArray = [
 	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/25299177_1331702946973862_228908318722312710_n.jpg?_nc_cat=0&oh=5d582d57413df4b30a234e02fecfc4dc&oe=5B61627E",
 	"https://www.facebook.com/photo.php?fbid=1225116010930463&set=a.269713153137425.55615.100002962278582&type=3&theater",
 	"https://www.facebook.com/photo.php?fbid=787267041480743&set=a.112545825619538.1073741826.100005923627989&type=3&theater",
-	"https://www.facebook.com/photo.php?fbid=486569175016299&set=pb.100009896624601.-2207520000.1523025656.&type=3&theater"
 	
 ];
 
