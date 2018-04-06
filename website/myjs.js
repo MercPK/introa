d16f13004d6fa477984d41ce5d44a9ba3d43690a
MercPK <coelhinhoninja@hotmail.com>
2018-04-06 12:58:38 -0400

    Update myjs.js

--- website/myjs.js
+++ website/myjs.js
@@ -1,7 +1,6 @@
 var imageArray = [
 	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/25299177_1331702946973862_228908318722312710_n.jpg?_nc_cat=0&oh=5d582d57413df4b30a234e02fecfc4dc&oe=5B61627E",
-	"https://www.facebook.com/photo.php?fbid=1225116010930463&set=a.269713153137425.55615.100002962278582&type=3&theater",
-	"https://www.facebook.com/photo.php?fbid=787267041480743&set=a.112545825619538.1073741826.100005923627989&type=3&theater",
+	"https://scontent.fijd1-1.fna.fbcdn.net/v/t1.0-9/19424142_1225116010930463_4218115255186064955_n.jpg?_nc_cat=0&oh=6e3b5b3c27886f47e7ab9f5c850b25b1&oe=5B714A7B",
 	
 ];
 
